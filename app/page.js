669a8a76656d0de92d202e4e5d16d41ccf203607
mahdee10 <diranimahdi2002@gmail.com>
2023-09-21 12:38:30 +0200

    asss

--- app/page.js
+++ app/page.js
@@ -4,6 +4,10 @@ import Header from './components/header'
 export default function Home() {
   return (
     <div>
+      <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       <Header></Header>
     </div>
   )

fb2de14fd3c5396f3c980f9feccd4360da407029
mahdee10 <diranimahdi2002@gmail.com>
2023-10-03 13:38:28 +0200

    adfsdad

--- app/components/footer.js
+++ app/components/footer.js
@@ -6,87 +6,106 @@ import l from "../../public/imgs/l.png"
 export default function Footer() {
     return (
         <div className='sm:pt-44 pt-20'>
-            
-            
+
+
             <div className="relative bg-my-green">
-            <div className="flex  justify-center 2 absolute left-1/2 transform -translate-x-1/2 top-0 transform -translate-y-1/2 ">
-                <div className="sm:py-10 bg-003A2C sm:w-fw sm:h-fhh sm:rounded-3xl rounded flex justify-center flex-col items-center w-80  p-5">
+                <div className="flex  justify-center 2 absolute left-1/2 transform -translate-x-1/2 top-0 transform -translate-y-1/2 ">
+                    <div className="sm:py-10 bg-003A2C sm:w-fw sm:h-fhh sm:rounded-3xl rounded flex justify-center flex-col items-center w-80  p-5">
 
-                    <h3 className="font-black text-white sm:text-3xl text-base  w-full text-center ">Register your CV now</h3>
-                    <p className="text-white sm:pt-5 pb-5 sm:text-base text-xxxs text-center pt-2">
-                        Lorarn ipsum dolor sit amnet, consoctotur ad piscing oft.
-                        Sod quis lacus non orci auismod -vostibulum vitoo ul ex.
-                    </p>
-                    <div className="">
-                        <button className=" text-white border-white sm:py-3 py-2 sm:px-5 px-1.5  border-2 rounded-lg w-fit sm:text-base text-xxxs">Register Now</button>
+                        <h3 className="font-black text-white sm:text-3xl text-base  w-full text-center ">Register your CV now</h3>
+                        <p className="text-white sm:pt-5 pb-5 sm:text-base text-xxxs text-center pt-2">
+                            Lorarn ipsum dolor sit amnet, consoctotur ad piscing oft.
+                            Sod quis lacus non orci auismod -vostibulum vitoo ul ex.
+                        </p>
+                        <div className="">
+                            <button className=" text-white border-white sm:py-3 py-2 sm:px-5 px-1.5  border-2 rounded-lg w-fit sm:text-base text-xxxs">Register Now</button>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="3  flex justify-between sm:px-24 px-5 sm:pt-8 sm:pb-12 mx-auto 2xl:container pb-10 overflow-hidden sm:pt-20 pt-10 flex-wrap">
-                <div className="flex items-center flex-col sm:pt-32 sm:w-80 w-full">
-                    <h3 className="font-black text-white sm:text-4xl text-2xl pt-14 w-full "><span className="text-black">Job</span>Now</h3>
-                    <p className="text-white sm:text-base  text-xxs sm:pt-5 pt-1 sm:pb-9 pb-2  sm:pr-10 pr-20">
-                        Lorarn ipsum dolor sit amnet, consoctotur ad piscing oft.
-                        Sod quis lacus non orci auismod
-                    </p>
-                </div>
-                <div className="flex  flex-col sm:pt-32 sm:w-fit w-54">
-                    <h3 className="font-black text-white sm:text-2xl text-xl sm:pt-14 pt-5 w-full ">Company</h3>
-                    <p className="text-white sm:text-base  text-xxs sm:pt-1 pt-1  pb-2  ">
-                        Address: 123 Fifth Avenue, New York-1060, USA
-                    </p>
-                    <p className="text-white sm:text-base text-xxs   pb-2  ">
-                        Call Us: +(1600)456 7800
-                    </p>
-                    <p className="text-white sm:text-base  text-xxs  pb-2  ">
-                        Email:yourid@example.com
-                    </p>
-                    <p className="text-white sm:text-base text-xxs  pb-2  ">
-                        Mon Sat: 9:00 AM - 19:00 PM
-                    </p>
-                </div>
+                <div className="3  flex justify-between sm:px-24 px-5 sm:pt-8 sm:pb-12 mx-auto 2xl:container pb-10 overflow-hidden sm:pt-20 pt-10 flex-wrap">
+                    <div className="flex items-center flex-col sm:pt-32 sm:w-80 w-full">
+                        <h3 className="font-black text-white sm:text-4xl text-2xl pt-14 w-full hover:text-black"><a href='/'><span className="text-black">Job</span>Now</a></h3>
+                        <p className="text-white sm:text-base  text-xxs sm:pt-5 pt-1 sm:pb-9 pb-2  sm:pr-10 pr-20">
+                            Lorarn ipsum dolor sit amnet, consoctotur ad piscing oft.
+                            Sod quis lacus non orci auismod
+                        </p>
+                    </div>
+                    <div className="flex  flex-col sm:pt-32 sm:w-fit w-54">
+                        <h3 className="font-black text-white sm:text-2xl text-xl sm:pt-14 pt-5 w-full ">Company</h3>
+                        <p className="text-white sm:text-base  text-xxs sm:pt-1 pt-1  pb-2  ">
+                            Address: 123 Fifth Avenue, New York-1060, USA
+                        </p>
+                        <a href="tel:+(1600)456 7800">
+                        <p className="text-white sm:text-base text-xxs   pb-2  hover:text-black">
+                            Call Us: +(1600)456 7800
+                        </p>
+                        </a>
+                       
+                        <a href="mailto:yourid@example.com">
+                            <p className="text-white sm:text-base  text-xxs  pb-2 hover:text-black ">
+                                Email:yourid@example.com
+                            </p>
+                        </a>
 
-                <div className="flex  flex-col sm:pt-32 sm:w-fit w-2/6">
-                    <h3 className="font-black text-white sm:text-2xl text-xl sm:pt-14 pt-5 w-full ">Pages</h3>
-                    <p className="text-white sm:text-base  text-xxs sm:pt-1 pt-1  pb-2  ">
-                    &gt; Home
-                    </p>
-                    <p className="text-white sm:text-base text-xxs   pb-1  ">
-                    &gt; Booking
-                    </p>
-                    <p className="text-white sm:text-base  text-xxs  pb-1  ">
-                    &gt;  Facilities
-                    </p>
-                    <p className="text-white sm:text-base text-xxs  pb-1  ">
-                    &gt;  About Us
-                    </p>
-                    <p className="text-white sm:text-base text-xxs   pb-1  ">
-                    &gt; Location
-                    </p>
-                    <p className="text-white sm:text-base text-xxs  pb-1  ">
-                    &gt; Contact
-                    </p>
-                </div>
-                <div className="flex  flex-col sm:pt-32 w-fit">
-                    <h3 className="font-black text-white sm:text-2xl text-xl sm:pt-14 pt-5 w-full ">Contact Us</h3>
-                    <div className="text-white sm:text-base  text-xxs sm:pt-3 pt-1  pb-2  flex">
+                        <p className="text-white sm:text-base text-xxs  pb-2  ">
+                            Mon Sat: 9:00 AM - 19:00 PM
+                        </p>
+                    </div>
+
+                    <div className="flex  flex-col sm:pt-32 sm:w-fit w-2/6">
+                        <h3 className="font-black text-white sm:text-2xl text-xl sm:pt-14 pt-5 w-full ">Pages</h3>
                         <a href='/'>
-                    <Image className=' sm:mr-3 mr-2 sm:w-8 sm:h-8 w-5 h-5' src={f} alt="Search Icon"></Image>
-                            
+                            <p className="text-white sm:text-base  text-xxs sm:pt-1 pt-1  pb-2  hover:text-black">
+                                &gt; Home
+                            </p>
                         </a>
                         <a href='/'>
-                        <Image className=' sm:mr-3 mr-2 sm:w-8 sm:h-8 w-5 h-5' src={l} alt="Search Icon"></Image>
-
+                            <p className="text-white sm:text-base text-xxs   pb-1  hover:text-black">
+                                &gt; Booking
+                            </p>
                         </a>
                         <a href='/'>
-                    <Image className=' sm:mr-3 mr-2 sm:w-8 sm:h-8 w-5 h-5' src={t} alt="Search Icon"></Image>
-
+                            <p className="text-white sm:text-base  text-xxs  pb-1  hover:text-black">
+                                &gt;  Facilities
+                            </p>
+                        </a>
+                        <a href='/'>
+                            <p className="text-white sm:text-base text-xxs  pb-1  hover:text-black">
+                                &gt;  About Us
+                            </p>
+                        </a>
+                        <a href='/'>
+                            <p className="text-white sm:text-base text-xxs   pb-1  hover:text-black">
+                                &gt; Location
+                            </p>
+                        </a>
+                        <a href='/'>
+                            <p className="text-white sm:text-base text-xxs  pb-1  hover:text-black">
+                                &gt; Contact
+                            </p>
                         </a>
 
                     </div>
-                    
-                </div>
+                    <div className="flex  flex-col sm:pt-32 w-fit">
+                        <h3 className="font-black text-white sm:text-2xl text-xl sm:pt-14 pt-5 w-full ">Contact Us</h3>
+                        <div className="text-white sm:text-base  text-xxs sm:pt-3 pt-1  pb-2  flex">
+                            <a href='/'>
+                                <Image className=' sm:mr-3 mr-2 sm:w-8 sm:h-8 w-5 h-5' src={f} alt="Search Icon"></Image>
+
+                            </a>
+                            <a href='/'>
+                                <Image className=' sm:mr-3 mr-2 sm:w-8 sm:h-8 w-5 h-5' src={l} alt="Search Icon"></Image>
+
+                            </a>
+                            <a href='/'>
+                                <Image className=' sm:mr-3 mr-2 sm:w-8 sm:h-8 w-5 h-5' src={t} alt="Search Icon"></Image>
+
+                            </a>
+
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
--- app/components/header.js
+++ app/components/header.js
@@ -32,7 +32,7 @@ export default function Header() {
                         <div className="sm:p-3 p-3 bg-white sm:rounded-lgg rounded-md flex justify-between ">
                             <div className='flex pl-2 bg-f9f9f9 sm:w-2/5 w-fit sm:rounded-lgg rounded-md sm:text-base text-xxxs items-center'>
                                 <Image className='mr-1  sm:w-fit sm:h-fit w-2 h-2' src={s} alt="Search Icon"></Image>
-                                <input className="sm:w-36 bg-f9f9f9 sm:pl-1  sm:py-3 py-2 rounded-md  sm:text-sm text-xxs w-32" type="text" value="Job title or Keyword" readOnly></input>
+                                <input className="sm:w-36 bg-f9f9f9 sm:pl-1  sm:py-3 py-2 rounded-md  sm:text-sm text-xxs w-28" type="text" value="Job title or Keyword" readOnly></input>
                             </div>
                             <div className='flex pl-2 bg-f9f9f9 sm:rounded-lgg  rounded-md sm:w-1/3 w-24 sm:text-base text-xxxs items-center'>
                                 <Image className='mr-1   sm:w-fit sm:h-fit w-2 h-2' src={location} alt="Search Icon"></Image>

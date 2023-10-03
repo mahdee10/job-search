687e5f1cf8f862d6a467fd2374e36aca886f6412
mahdee10 <diranimahdi2002@gmail.com>
2023-10-03 11:01:09 +0200

    adfa

--- app/components/footer.js
+++ app/components/footer.js
@@ -25,52 +25,52 @@ export default function Footer() {
             </div>
             <div className="3  flex justify-between sm:px-24 px-12 sm:pt-8 sm:pb-12 mx-auto 2xl:container pb-10 overflow-hidden sm:pt-20 pt-10 flex-wrap">
                 <div className="flex items-center flex-col sm:pt-32 sm:w-80 w-full">
-                    <h3 className="font-black text-white sm:text-3xl text-2xl pt-14 w-full "><span className="text-black">Job</span>Now</h3>
-                    <p className="text-white sm:text-xs  text-xxxs sm:pt-5 pt-1 sm:pb-9 pb-2  sm:pr-10 pr-20">
+                    <h3 className="font-black text-white sm:text-4xl text-2xl pt-14 w-full "><span className="text-black">Job</span>Now</h3>
+                    <p className="text-white sm:text-base  text-xxxs sm:pt-5 pt-1 sm:pb-9 pb-2  sm:pr-10 pr-20">
                         Lorarn ipsum dolor sit amnet, consoctotur ad piscing oft.
                         Sod quis lacus non orci auismod
                     </p>
                 </div>
                 <div className="flex  flex-col sm:pt-32 sm:w-fit w-54">
-                    <h3 className="font-black text-white sm:text-xl text-xs sm:pt-14 pt-5 w-full ">Company</h3>
-                    <p className="text-white sm:text-xs  text-xxxs sm:pt-1 pt-1  pb-2  ">
+                    <h3 className="font-black text-white sm:text-2xl text-xs sm:pt-14 pt-5 w-full ">Company</h3>
+                    <p className="text-white sm:text-base  text-xxxs sm:pt-1 pt-1  pb-2  ">
                         Address: 123 Fifth Avenue, New York-1060, USA
                     </p>
-                    <p className="text-white sm:text-xs text-xxxs   pb-2  ">
+                    <p className="text-white sm:text-base text-xxxs   pb-2  ">
                         Call Us: +(1600)456 7800
                     </p>
-                    <p className="text-white sm:text-xs  text-xxxs  pb-2  ">
+                    <p className="text-white sm:text-base  text-xxxs  pb-2  ">
                         Email:yourid@example.com
                     </p>
-                    <p className="text-white sm:text-xs text-xxxs  pb-2  ">
+                    <p className="text-white sm:text-base text-xxxs  pb-2  ">
                         Mon Sat: 9:00 AM - 19:00 PM
                     </p>
                 </div>
 
                 <div className="flex  flex-col sm:pt-32 sm:w-fit w-2/6">
-                    <h3 className="font-black text-white sm:text-xl text-xs sm:pt-14 pt-5 w-full ">Pages</h3>
-                    <p className="text-white sm:text-xs  text-xxxs sm:pt-1 pt-1  pb-2  ">
+                    <h3 className="font-black text-white sm:text-2xl text-xs sm:pt-14 pt-5 w-full ">Pages</h3>
+                    <p className="text-white sm:text-base  text-xxxs sm:pt-1 pt-1  pb-2  ">
                     &gt; Home
                     </p>
-                    <p className="text-white sm:text-xs text-xxxs   pb-1  ">
+                    <p className="text-white sm:text-base text-xxxs   pb-1  ">
                     &gt; Booking
                     </p>
-                    <p className="text-white sm:text-xs  text-xxxs  pb-1  ">
+                    <p className="text-white sm:text-base  text-xxxs  pb-1  ">
                     &gt;  Facilities
                     </p>
-                    <p className="text-white sm:text-xs text-xxxs  pb-1  ">
+                    <p className="text-white sm:text-base text-xxxs  pb-1  ">
                     &gt;  About Us
                     </p>
-                    <p className="text-white sm:text-xs text-xxxs   pb-1  ">
+                    <p className="text-white sm:text-base text-xxxs   pb-1  ">
                     &gt; Location
                     </p>
-                    <p className="text-white sm:text-xs text-xxxs  pb-1  ">
+                    <p className="text-white sm:text-base text-xxxs  pb-1  ">
                     &gt; Contact
                     </p>
                 </div>
                 <div className="flex  flex-col sm:pt-32 w-fit">
-                    <h3 className="font-black text-white sm:text-xl text-xs sm:pt-14 pt-5 w-full ">Contact Us</h3>
-                    <div className="text-white sm:text-xs  text-xxxs sm:pt-3 pt-1  pb-2  flex">
+                    <h3 className="font-black text-white sm:text-2xl text-xs sm:pt-14 pt-5 w-full ">Contact Us</h3>
+                    <div className="text-white sm:text-base  text-xxxs sm:pt-3 pt-1  pb-2  flex">
                     <Image className=' sm:mr-3 mr-1 sm:w-fit sm:h-fit w-2 h-2' src={f} alt="Search Icon"></Image>
                     <Image className=' sm:mr-3 mr-1 sm:w-fit sm:h-fit w-2 h-2' src={l} alt="Search Icon"></Image>
                     <Image className=' sm:mr-3 mr-1 sm:w-fit sm:h-fit w-2 h-2' src={t} alt="Search Icon"></Image>
